40de9257b2c00ce13b3dac4b21ebaff91bb35a9a
aryan <aryanneogi@gmail.com>
2024-02-23 21:51:49 +0530

    Added animations to home page

---
+++ home.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["HTML.","CSS.","JavaScript.","BootStrap."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
